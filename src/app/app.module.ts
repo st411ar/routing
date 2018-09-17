c8a6345ecd75965d238bb6ade303932222d66967
Vitaly Kiselev <st411ar@ya.ru>
2018-09-17 17:31:57 +0300

    created login routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {
+  APP_BASE_HREF,
+  HashLocationStrategy,
+  LocationStrategy
+} from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import {APP_BASE_HREF, HashLocationStrategy, LocationStrategy} from '@angular/common';
 import { LoginComponent } from './login/login.component';
 
 
@@ -15,7 +19,8 @@ const routes: Routes = [
   { path: 'home',         component: HomeComponent },
   { path: 'about',        component: AboutComponent },
   { path: 'contact',      component: ContactComponent },
-  { path: 'contactus',    redirectTo: 'contact' }
+  { path: 'contactus',    redirectTo: 'contact' },
+  { path: 'login',        component: LoginComponent }
 ];
 
 
