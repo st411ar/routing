bb6f70e24af81221979963a072d13af9fc35920b
Vitaly Kiselev <st411ar@ya.ru>
2018-09-18 13:56:11 +0300

    created products more-info component with routing

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { MoreInfoComponent } from './more-info/more-info.component';
 
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'main', pathMatch: 'full' },
-	{ path: 'main', component: MainComponent }
+	{ path: 'main', component: MainComponent },
+	{ path: 'more-info', component: MoreInfoComponent }
 ];
 
 @NgModule({
@@ -14,6 +16,9 @@ export const routes: Routes = [
 		CommonModule,
 		RouterModule
 	],
-	declarations: [MainComponent]
+	declarations: [
+		MainComponent,
+		MoreInfoComponent
+	]
 })
 export class ProductsModule { }
