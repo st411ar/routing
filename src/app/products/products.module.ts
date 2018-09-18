4e09b5ab335252fc6eeed9d52fb6fafbc44896bf
Vitaly Kiselev <st411ar@ya.ru>
2018-09-18 13:51:02 +0300

    created products main component with routing

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'main', pathMatch: 'full' },
+	{ path: 'main', component: MainComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+	imports: [
+		CommonModule,
+		RouterModule
+	],
+	declarations: [MainComponent]
 })
 export class ProductsModule { }
